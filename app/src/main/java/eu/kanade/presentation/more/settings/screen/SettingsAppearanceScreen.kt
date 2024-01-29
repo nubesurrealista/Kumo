ee004b7d41ee1eb401a7a0b6cac165bc37ffb9dc
Mathis CHAPUIS <mathis.chapuis02@gmail.com>
2024-01-29 12:57:13 +0200

    Add a font size parameter (#207)

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAppearanceScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsAppearanceScreen.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
@@ -20,6 +19,7 @@ import eu.kanade.presentation.more.settings.widget.AppThemeModePreferenceWidget
 import eu.kanade.presentation.more.settings.widget.AppThemePreferenceWidget
 import eu.kanade.tachiyomi.util.system.toast
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.collections.immutable.toImmutableMap
 import tachiyomi.i18n.MR
 import tachiyomi.presentation.core.i18n.stringResource
@@ -116,11 +116,21 @@ object SettingsAppearanceScreen : SearchableSettings {
                     onClick = { navigator.push(AppLanguageScreen()) },
                 ),
                 Preference.PreferenceItem.ListPreference(
+                    pref = uiPreferences.fontSize(),
+                    title = stringResource(MR.strings.pref_font_size),
+                    entries = persistentMapOf(
+                        0.75f to stringResource(MR.strings.pref_font_size_small),
+                        1f to stringResource(MR.strings.pref_font_size_normal),
+                        1.25f to stringResource(MR.strings.pref_font_size_large),
+                        1.5f to stringResource(MR.strings.pref_font_size_huge),
+                    ),
+                ),
+                Preference.PreferenceItem.ListPreference(
                     preference = uiPreferences.tabletUiMode(),
+                    title = stringResource(MR.strings.pref_tablet_ui_mode),
                     entries = TabletUiMode.entries
                         .associateWith { stringResource(it.titleRes) }
                         .toImmutableMap(),
-                    title = stringResource(MR.strings.pref_tablet_ui_mode),
                     onValueChanged = {
                         context.toast(MR.strings.requires_app_restart)
                         true
