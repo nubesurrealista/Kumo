c3afae0622d1050b77e51a6f600c4ae4b994cfd7
nube <nubesurrealista@proton.me>
2025-07-14 13:23:54 -0400

    fix dracula

--- app/src/main/java/eu/kanade/tachiyomi/ui/base/delegate/ThemingDelegate.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/base/delegate/ThemingDelegate.kt
@@ -31,7 +31,7 @@ class ThemingDelegateImpl : ThemingDelegate {
 private val themeResources: Map<AppTheme, Int> = mapOf(
     AppTheme.MONET to R.style.Theme_Tachiyomi_Monet,
     AppTheme.CATPPUCCIN to R.style.Theme_Tachiyomi_Catppuccin,
-    AppTheme.DACULA to R.style.Theme_Tachiyomi_Dracula,
+    AppTheme.DRACULA to R.style.Theme_Tachiyomi_Dracula,
     AppTheme.GREEN_APPLE to R.style.Theme_Tachiyomi_GreenApple,
     AppTheme.LAVENDER to R.style.Theme_Tachiyomi_Lavender,
     AppTheme.MIDNIGHT_DUSK to R.style.Theme_Tachiyomi_MidnightDusk,
