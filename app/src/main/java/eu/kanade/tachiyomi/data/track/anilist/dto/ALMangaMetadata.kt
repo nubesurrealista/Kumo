0bf38c7e99a0239a0fc4f948c5ae81e20a72096c
Nube 云 <136946098+nubesurrealista@users.noreply.github.com>
2026-08-17 23:17:04 -0400

    Fix ALMangaMetadata.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/dto/ALMangaMetadata.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/anilist/dto/ALMangaMetadata.kt
@@ -21,10 +21,4 @@ data class ALMangaMetadataMedia(
     val coverImage: ItemCover,
     val description: String?,
     val staff: ALStaff,
-    val format: String? = null,
-    val countryOfOrigin: String? = null,
-    val status: String? = null,
-    val chapters: Int? = null,
-    val averageScore: Int? = null,
-    val startDate: ALDate? = null,
 )
